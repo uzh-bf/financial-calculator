00274fd996d4a4457dec205e5a0f4838a2a5fc40
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-03-01 20:36:43 +0100

    Extract black scholes into separate module

---
+++ calc/black_scholes.py
@@ -0,0 +1,25 @@
+import numpy as np
+import scipy.stats as si
+
+
+def compute_black_scholes(S, K, T, r, q, sigma):
+    # see https://aaronschlegel.me/black-scholes-formula-python.html
+
+    # S: spot price
+    # K: strike price
+    # T: time to maturity
+    # r: interest rate
+    # q: rate of continuous dividend paying asset
+    # sigma: volatility of underlying asset
+
+    d1 = (np.log(S / K) + (r - q + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - q - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+
+    call = S * np.exp(-q * T) * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(
+        -r * T
+    ) * si.norm.cdf(d2, 0.0, 1.0)
+    put = K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0) - S * np.exp(
+        -q * T
+    ) * si.norm.cdf(-d1, 0.0, 1.0)
+
+    return np.round(put, 2), np.round(call, 2), np.round(d1, 2), np.round(d2, 2)
--- calc/main.py
+++ calc/main.py
@@ -1,5 +1,4 @@
-import numpy as np
-import scipy.stats as si
+import black_scholes
 
 from pydantic import BaseModel
 from fastapi import FastAPI
@@ -26,36 +25,13 @@ class BlackScholesInput(BaseModel):
     dividend: float
 
 
-def black_scholes_call_div(S, K, T, r, q, sigma):
-    # see https://aaronschlegel.me/black-scholes-formula-python.html
-
-    # S: spot price
-    # K: strike price
-    # T: time to maturity
-    # r: interest rate
-    # q: rate of continuous dividend paying asset
-    # sigma: volatility of underlying asset
-
-    d1 = (np.log(S / K) + (r - q + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
-    d2 = (np.log(S / K) + (r - q - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
-
-    call = S * np.exp(-q * T) * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(
-        -r * T
-    ) * si.norm.cdf(d2, 0.0, 1.0)
-    put = K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0) - S * np.exp(
-        -q * T
-    ) * si.norm.cdf(-d1, 0.0, 1.0)
-
-    return np.round(put, 2), np.round(call, 2), np.round(d1, 2), np.round(d2, 2)
-
-
 @app.post("/black_scholes")
 def calc_black_scholes(input: BlackScholesInput):
     result = dict()
 
     series = list()
     for i in range(1, int(input.strike * 2)):
-        put_price, call_price, d1, d2 = black_scholes_call_div(
+        put_price, call_price, d1, d2 = black_scholes.compute_black_scholes(
             i,
             input.strike,
             input.maturity,
@@ -68,7 +44,7 @@ def calc_black_scholes(input: BlackScholesInput):
         )
     result["series"] = series
 
-    specific_result = black_scholes_call_div(
+    specific_result = black_scholes.compute_black_scholes(
         input.underlying,
         input.strike,
         input.maturity,
